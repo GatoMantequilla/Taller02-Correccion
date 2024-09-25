eb8640875771078d68767e9a2be845098796e6f8
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-09-25 10:37:00 -0300

    Actualización: creación de pruebas unitarias

--- Taller/src/test/java/RegistroTest.java
+++ Taller/src/test/java/RegistroTest.java
@@ -1,7 +1,32 @@
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 
-class Taller02Test {
+class RegistroTest {
 
+    @org.junit.jupiter.api.Test
+    void crearRegistro() {
+    }
 
+    @org.junit.jupiter.api.Test
+    void mostrarMayoresDeEdad() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mostrarMenoresDeEdad() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void conteoDeSolteros() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void conteoDeCasados() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void obtenerUltimoEspacio() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hayCupo() {
+    }
 }
